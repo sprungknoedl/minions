f96fd025d87330a225b5b61e087ca8aace50665d
Thomas Kastner <thomas@nimbusec.com>
2017-07-06 09:38:01 +0200

    default funcmap, fixed Funcs to actually work

--- formats.go
+++ formats.go
@@ -3,6 +3,7 @@ package minions
 import (
 	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"html/template"
 	"io/ioutil"
 	"net/http"
@@ -50,18 +51,37 @@ type Templates struct {
 // are reloaded on each request.
 func NewTemplates(dir string, reload bool) *Templates {
 	return &Templates{
-		dir:     dir,
-		funcmap: template.FuncMap{},
-		reload:  reload,
+		dir: dir,
+		funcmap: template.FuncMap{
+			"div": func(dividend, divisor int) float64 {
+				return float64(dividend) / float64(divisor)
+			},
+			"dict": func(values ...interface{}) (map[string]interface{}, error) {
+				if len(values)%2 != 0 {
+					return nil, errors.New("invalid dict call")
+				}
+				dict := make(map[string]interface{}, len(values)/2)
+				for i := 0; i < len(values); i += 2 {
+					key, ok := values[i].(string)
+					if !ok {
+						return nil, errors.New("dict keys must be strings")
+					}
+					dict[key] = values[i+1]
+				}
+				return dict, nil
+			},
+		},
+		reload: reload,
 	}
 }
 
 // Funcs adds the elements of the argument map to the template's function map.
-// It panics if a value in the map is not a function with appropriate return
-// type. However, it is legal to overwrite elements of the map. The return
-// value is the template, so calls can be chained.
+// The return value is the template, so calls can be chained.
 func (tpl *Templates) Funcs(funcmap template.FuncMap) *Templates {
-	tpl.tpl.Funcs(template.FuncMap(funcmap))
+	for name, fn := range funcmap {
+		tpl.funcmap[name] = fn
+	}
+
 	return tpl
 }
 
